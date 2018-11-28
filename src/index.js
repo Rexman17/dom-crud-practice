714dffc5c3d2640a46770a37e2f70ae593eab6ce
Rexman17 <a.stone1794@gmail.com>
2018-11-28 10:11:07 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -8,55 +8,69 @@ document.addEventListener('DOMContentLoaded', () => { // event object is optiona
   const searchBox = document.getElementById('search-box') // make use of this in the search deliverable
 // ********** VARIABLES ***************
   const giftsURL = 'http://localhost:3000/gifts'
- // fill this in the get request
- let allGifts = ''
+   // fill this in the get request
+   let allGifts = ''
 // ********** FETCHES & EVENT LISTENERS **********
   fetch(giftsURL, { method: 'GET' })
-    .then(r => r.json())
-    .then(arrayOfGiftObjects => {
-      allGifts = arrayOfGiftObjects
-      console.log(allGifts);
-      // console.log(parsedJSON);
-      arrayOfGiftObjects.forEach((giftObject) => {
-        giftsContainer.innerHTML += `<div data-id="${giftObject.id}">
-                                      <h3>${giftObject.name}</h3>
-                                      <img src=${giftObject.image}>
-                                      <button data-id="${giftObject.id}" class="edit" type="button">Edit Me!</button>
-                                      <button data-id="${giftObject.id}" class="delete" type="button">Delete</button>
-                                      </div>
-                                      `
-      })
-    }) // end of fetch
+  .then(r => r.json())
+  .then(arrayOfGiftObjects => {
+    allGifts = arrayOfGiftObjects
+    console.log("initially got all gifts");
+    console.log("all gifts are", allGifts);
+    console.log("-------");
+    // console.log(allGifts);
+    // console.log(parsedJSON);
+    arrayOfGiftObjects.forEach((giftObject) => {
+      giftsContainer.innerHTML += `<div id="gift-${giftObject.id}">
+      <h3>${giftObject.name}</h3>
+      <img src=${giftObject.image}>
+      <button data-id="${giftObject.id}" class="edit" type="button">Edit Me!</button>
+      <button data-id="${giftObject.id}" class="delete" type="button">Delete</button>
+      </div>
+      `
+    })
+  }) // end of fetch
 
     // event listener for CLICKING the EDIT BUTTON OR DELETE
     // we didn't update our data yet!
     giftsContainer.addEventListener('click', e => {
       const clickedGiftId = e.target.dataset.id
       if (e.target.className === 'edit') { // when we click EDIT button
-        fetch(`http://localhost:3000/gifts/${clickedGiftId}`)
-          .then(r => r.json())
-          .then(foundGift => {
+        let foundGift = allGifts.find(gift => gift.id == clickedGiftId) // gift obj from array of objs retreived from original fetch
+        // fetch(`http://localhost:3000/gifts/${clickedGiftId}`)
+          // .then(r => r.json())
+          // .then(foundGift => {
             // find input fields for the form and pre fill them
             giftNameInput.value = foundGift.name
             giftImgInput.value = foundGift.image
             editGiftForm.dataset.id = clickedGiftId
-            console.log(editGiftForm.dataset.id); // save the id of the clicked image in a dataset on the edit form u will then submit on
-          })
+        //     console.log(editGiftForm.dataset.id); // save the id of the clicked image in a dataset on the edit form u will then submit on
+        //   })
       } else if (e.target.className === 'delete') {
+        giftsContainer.querySelector(`#gift-${clickedGiftId}`).remove() // optimistically render the edit
 
-        giftsContainer.querySelector(`[data-id='${clickedGiftId}']`).remove() // optimistically render the edit
+        console.log("deleting gift")
+        console.log("all gifts arr is", allGifts)
+        console.log("------");
 
         fetch(`http://localhost:3000/gifts/${clickedGiftId}`, {
           method: 'DELETE'
         })
-        .then(r => r.json())
-        .then(console.log) // pessimistically would be inside this then
+          .then(r => r.json())
+          // .then(json => {
+          //   debugger
+          // }) // pessimistically would be inside this then
       }
+      // debugger
+      console.log(allGifts);
     }) // end of edit click event listener
 
     // patching/fetch event listener for when u submit on edit form
     editGiftForm.addEventListener('submit', e => {
       e.preventDefault()
+      console.log("editing gift")
+      console.log("all gifts arr is", allGifts)
+      console.log("------");
       // console.log(e);
       fetch(`http://localhost:3000/gifts/${e.target.dataset.id}`, { // id stored in the form itself that we assigned with editGiftForm.dataset.id = clickedGiftId
         method: 'PATCH',
@@ -73,8 +87,8 @@ document.addEventListener('DOMContentLoaded', () => { // event object is optiona
       .then(editedGiftObj => {
         // console.log(editedGiftObj);
         // append to DOM - find the one gift and update it
-        console.log(editedGiftObj.id);
-        let divOfGiftToUpdate = document.getElementById(`${editedGiftObj.id}`)
+        // console.log(editedGiftObj.id);
+        let divOfGiftToUpdate = document.querySelector(`#gift-${editedGiftObj.id}`)
         divOfGiftToUpdate.querySelector('h3').innerText = editedGiftObj.name
         divOfGiftToUpdate.querySelector('img').src = editedGiftObj.image
       })
@@ -84,21 +98,29 @@ document.addEventListener('DOMContentLoaded', () => { // event object is optiona
       searchBox.addEventListener('input', e => {
         const userInput = e.target.value
         // console.log(userInput);
-        const filteredGifts = allGifts.filter((gift) => {
-          return gift.name.includes(userInput) // filtered the array in memory
+        fetch(giftsURL)
+        .then(res => res.json())
+        .then(json => {
+          allGifts = json
+          const filteredGifts = allGifts.filter((gift) => {
+            return gift.name.includes(userInput) // filtered the array in memory
+          })
+          console.log("all gifts are", allGifts);
+          console.log("filtered gifts are", filteredGifts);
+          console.log("-----------");
+          // now render it in the DOM!
+          const giftHTML = filteredGifts.map((giftObject) => {
+            return `
+            <div data-id="${giftObject.id}">
+            <h3>${giftObject.name}</h3>
+            <img src=${giftObject.image}>
+            <button data-id="${giftObject.id}" class="edit" type="button">Edit Me!</button>
+            <button data-id="${giftObject.id}" class="delete" type="button">Delete</button>
+            </div>
+            `
+          })
+          giftsContainer.innerHTML = giftHTML.join('')
         })
-        // now render it in the DOM!
-        const giftHTML = filteredGifts.map((giftObject) => {
-          return `
-                  <div data-id="${giftObject.id}">
-                    <h3>${giftObject.name}</h3>
-                    <img src=${giftObject.image}>
-                    <button data-id="${giftObject.id}" class="edit" type="button">Edit Me!</button>
-                    <button data-id="${giftObject.id}" class="delete" type="button">Delete</button>
-                  </div>
-                  `
-        })
-        giftsContainer.innerHTML = giftHTML.join('')
       }) // search box event listener
 
 }) // end of DOMContentLoaded
